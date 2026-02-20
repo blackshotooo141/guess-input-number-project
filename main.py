76248a590550bc6db336db54fcdd58640092155c
James Margolis <margolisjames151@gmail.com>
2026-02-20 13:57:36 +0300

    Added checking to continue playing and fixed mistakes

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ def main():
 
     while playing:
         #Компьютер загадывает случайное число
-        mystery_number = randint(-100, 100)
+        mystery_number = randint(1, 100)
         
-        print('Компьютер загадал случайное целое число в диапазоне от -100 до 100.')
+        print('Компьютер загадал случайное целое число в диапазоне от 1 до 100.')
         print('Постарайтесь угадать его!')
 
         number_is_classified = True
@@ -19,17 +19,25 @@ def main():
             guess_input_number = int(input())
 
             if (guess_input_number < mystery_number):
-                print('Введённое число меньше! Попробуйте снова ;)')
+                print('Введённое число больше! Попробуйте снова ;)')
                 continue
             elif (guess_input_number > mystery_number):
-                print('Введённое число больше! Подумайте лучше.')
+                print('Введённое число меньше! Подумайте лучше.')
                 continue
             else:
                 print('Ура! Вы угадали число компьютера)')
+                number_is_classified = False
+
+        #Программа предлагает пользователю сыграть снова
+        print('Хотите сыграть ещё раз? Введите play для продолжения!')
+        continue_playing = input()[:5]
+
+        if (continue_playing == 'play'):
+            continue
+        else:
+            playing = False
+            print('Спасибо за игру!')
 
 
-        print('Хотите ли вы продолжить игру? y/n')
-
-
-if '__name__ ' == '__main___':
+if __name__ == '__main__':
     main()
